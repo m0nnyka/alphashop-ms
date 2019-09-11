c3b5b79d40fb5d8fe8b51f49e8ca442ef56942d1
mopetkova.si2001@gmail.com <moperkova.si2001@gmail.com>
2019-09-11 16:09:43 +0200

    init

--- src/main/java/com/xantrix/webapp/Application.java
+++ src/main/java/com/xantrix/webapp/Application.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+//per abilitare spring cloud server: EnableConfigServer
 @EnableConfigServer
 @SpringBootApplication
 public class Application
